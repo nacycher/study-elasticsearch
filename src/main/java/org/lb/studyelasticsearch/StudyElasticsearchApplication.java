19174ee7651b2491b14e7667a56143021289bbd2
nacycher <nacycher@163.com>
2025-02-05 11:25:21 +0800

    init project

---
+++ src/main/java/org/lb/studyelasticsearch/StudyElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package org.lb.studyelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyElasticsearchApplication.class, args);
+    }
+
+}
