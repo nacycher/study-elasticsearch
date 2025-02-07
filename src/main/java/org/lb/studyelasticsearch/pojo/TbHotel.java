0fe81a3aa5f91058b05d1ad3882f3784fcde2f16
nacycher <nacycher@163.com>
2025-02-07 21:05:56 +0800

    [fix]mapper.xml 配置问题

--- src/main/java/org/lb/studyelasticsearch/pojo/TbHotel.java
+++ src/main/java/org/lb/studyelasticsearch/pojo/TbHotel.java
@@ -1,6 +1,9 @@
 package org.lb.studyelasticsearch.pojo;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
@@ -10,7 +13,9 @@ import org.apache.commons.lang3.builder.ToStringStyle;
  * @author ruoyi
  * @date 2025-02-06
  */
-@TableName("tb_hotel")
+@Getter
+@Setter
+@ToString
 public class TbHotel
 {
     private static final long serialVersionUID = 1L;
@@ -46,146 +51,8 @@ public class TbHotel
     private String latitude;
 
     /** 经度;例: 120.3925 */
-    private String Longitude;
+    private String longitude;
 
     /** 酒店图片；例： / img / 1.jpg */
     private String pic;
-
-    public void setId(Long id) 
-    {
-        this.id = id;
-    }
-
-    public Long getId() 
-    {
-        return id;
-    }
-
-    public void setName(String name) 
-    {
-        this.name = name;
-    }
-
-    public String getName() 
-    {
-        return name;
-    }
-
-    public void setAddress(String address) 
-    {
-        this.address = address;
-    }
-
-    public String getAddress() 
-    {
-        return address;
-    }
-
-    public void setPrice(Long price) 
-    {
-        this.price = price;
-    }
-
-    public Long getPrice() 
-    {
-        return price;
-    }
-
-    public void setScore(Long score) 
-    {
-        this.score = score;
-    }
-
-    public Long getScore() 
-    {
-        return score;
-    }
-
-    public void setBrand(String brand) 
-    {
-        this.brand = brand;
-    }
-
-    public String getBrand() 
-    {
-        return brand;
-    }
-
-    public void setCity(String city) 
-    {
-        this.city = city;
-    }
-
-    public String getCity() 
-    {
-        return city;
-    }
-
-    public void setStarName(String starName) 
-    {
-        this.starName = starName;
-    }
-
-    public String getStarName() 
-    {
-        return starName;
-    }
-
-    public void setBusiness(String business) 
-    {
-        this.business = business;
-    }
-
-    public String getBusiness() 
-    {
-        return business;
-    }
-
-    public void setLatitude(String latitude) 
-    {
-        this.latitude = latitude;
-    }
-
-    public String getLatitude() 
-    {
-        return latitude;
-    }
-
-    public void setLongitude(String Longitude) 
-    {
-        this.Longitude = Longitude;
-    }
-
-    public String getLongitude() 
-    {
-        return Longitude;
-    }
-
-    public void setPic(String pic) 
-    {
-        this.pic = pic;
-    }
-
-    public String getPic() 
-    {
-        return pic;
-    }
-
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("id", getId())
-            .append("name", getName())
-            .append("address", getAddress())
-            .append("price", getPrice())
-            .append("score", getScore())
-            .append("brand", getBrand())
-            .append("city", getCity())
-            .append("starName", getStarName())
-            .append("business", getBusiness())
-            .append("latitude", getLatitude())
-            .append("Longitude", getLongitude())
-            .append("pic", getPic())
-            .toString();
-    }
 }
